8ffbc32e11ca3cd03b992bbf6549e16c450c4b55
Baris0 <barisseckin0@gmail.com>
2022-03-15 17:37:53 +0300

    category and register-login update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
-import Newests from "./pages/Newests";
 import Trends from "./pages/Trends";
-import MostPopular from "./pages/MostPopular";
 import Navi from "./layouts/Navi";
 import Profile from "./pages/Profile";
 import Notifications from "./pages/Notifications";
@@ -19,9 +17,7 @@ function App() {
           <Route path="/register" element={<Register />}></Route>
           <Route path="/" element={<Home />}></Route>
           <Route path="/login" element={<Login />} />
-          <Route path="/newests" element={<Newests />} />
           <Route path="/trends" element={<Trends />} />
-          <Route path="/popular" element={<MostPopular />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/notifications" element={<Notifications />} />
         </Routes> 
